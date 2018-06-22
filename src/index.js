269867b310f75fa8cfb84df76b1ece11964a7321
peggypeggyq <qiu.p@husky.neu.edu>
2018-06-22 14:53:46 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 ReactDOM.render(
-    <h1>Food Truck Tracker</h1>,
-    <h3>Designer @ Joann</h3>,
-    <h3>Developer @ Peiran</h3>,
+    <h1>Food Truck Tracker</h1>
+    <h3>Designer: Joann</h3>
+    <h3>Developer: Peiran</h3>,
     document.getElementById('root')
 );
